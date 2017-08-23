e096dbbf54623e02125eb0271c4230e3dba806df
roy.ren <roy.ren@yeahmobi.com>
2017-08-23 19:31:16 +0800

    1.upload

--- src/Handler.java
+++ src/Handler.java
@@ -2,5 +2,17 @@
  * 负责发送和处理消息
  */
 public class Handler {
+    // 消息队列
+    MessageQueue messageQueue;
 
+    // 消息泵
+    Looper looper;
+
+    public Handler() {
+        looper = Looper.getLocalLooper();
+        if (looper == null){
+            throw new RuntimeException("Can't create handler inside thread that has not called Looper.prepare()");
+        }
+        messageQueue = looper.messageQueue;
+    }
 }
--- src/Looper.java
+++ src/Looper.java
@@ -3,11 +3,46 @@
  */
 public class Looper {
 
-    ThreadLocal<Message> messageThreadLocal = new ThreadLocal<>();
+    static ThreadLocal<Looper> messageThreadLocal = new ThreadLocal<>();
+
     MessageQueue messageQueue;
 
-    private Looper(){
+    private Looper() {
         messageQueue = new MessageQueue();
+    }
+
+    /**
+     * 获取Looper的线程副本
+     *
+     * @return
+     */
+    public static Looper getLocalLooper() {
+        // 返回此线程局部变量的当前线程副本中的值，如果这是线程第一次调用该方法，则创建并初始化此副本。
+        return messageThreadLocal.get();
+    }
+
+    public static void prepare() {
+        if (messageThreadLocal.get() != null) {
+            throw new RuntimeException("Only one Looper may be created per thread");
+        }
+        messageThreadLocal.set(new Looper());
+    }
+
+    public static void loop() {
+        Looper looper = getLocalLooper();
+        if (looper == null) {
+            throw new RuntimeException("No Looper; Looper.prepare() wasn't called on this thread.");
+        }
+
+        MessageQueue messageQueue = looper.messageQueue;
+        // 无限循环
+        for (;;){
+            Message message = messageQueue.consumeMessage();
+            if (message == null){
+                continue;
+            }
+
+        }
     }
 
 
